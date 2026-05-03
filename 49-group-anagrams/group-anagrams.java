43ae2d5bdf68b2e2fb61efb1bc6fced62f8c1eb3
mounith <vtu25939@veltech.edu.in>
2026-05-03 20:09:08 +0530

    Time: 7 ms (74.18%) | Memory: 49.7 MB (60.50%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -2,25 +2,13 @@ import java.util.*;
 
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        if (strs == null || strs.length == 0) return new ArrayList<>();
-        
-        // Map: Sorted string (Key) -> List of original anagrams (Value)
-        Map<String, List<String>> map = new HashMap<>();
-        
-        for (String s : strs) {
-            // Convert to char array, sort it, and convert back to string
-            char[] ca = s.toCharArray();
-            Arrays.sort(ca);
-            String key = String.valueOf(ca);
-            
-            // If the key isn't in the map, add a new list
-            if (!map.containsKey(key)) {
-                map.put(key, new ArrayList<>());
-            }
-            // Add the original string to the corresponding anagram group
-            map.get(key).add(s);
-        }
-        
-        return new ArrayList<>(map.values());
-    }
-}
+            Map<String, List<String>> map = new HashMap<>();
+                    for (String s : strs) {
+                                char[] ch = s.toCharArray();
+                                            Arrays.sort(ch);
+                                                        map.computeIfAbsent(new String(ch), k -> new ArrayList<>()).add(s);
+                                                                }
+                                                                        return new ArrayList<>(map.values());
+                                                                            }
+                                                                            }
+                                                                            

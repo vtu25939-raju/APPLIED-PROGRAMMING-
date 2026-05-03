2a3be8c41213eeadaac78082d22478f69c021cb7
mounith <vtu25939@veltech.edu.in>
2026-05-03 19:17:13 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (83.95%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,20 +1,21 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        
-        return -1;
-    }
-}
+        public int search(int[] nums, int target) {
+                int left = 0;
+                        int right = nums.length - 1;
+
+                                while (left <= right) {
+                                            // Find the middle index (prevents overflow)
+                                                        int mid = left + (right - left) / 2;
+
+                                                                    if (nums[mid] == target) {
+                                                                                    return mid; // Target found
+                                                                                                } else if (nums[mid] < target) {
+                                                                                                                left = mid + 1; // Look in the right half
+                                                                                                                            } else {
+                                                                                                                                            right = mid - 1; // Look in the left half
+                                                                                                                                                        }
+                                                                                                                                                                }
+
+                                                                                                                                                                        return -1; // Target not found
+                                                                                                                                                                            }
+                                                                                                                                                                            }
